bb5e227ed6b4f3dcc3dad21b2047e9ccd0c9a8f4
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2016-01-20 19:26:58 +0200

    Add info on how to get the latest commit version

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-_pkgname=steamos-xpad
-_pkgver=0.1
-_pkgpath=/usr/src/${_pkgname}-${_pkgver}
+# git clone https://github.com/ValveSoftware/steamos_kernel.git
+# cd steamos_kernel
+# echo "$(git rev-parse --abbrev-ref HEAD).$(git log -n 1 --pretty=format:%h -- drivers/input/joystick/xpad.c)" | sed 's/-/./g'_pkgname=steamos-xpad
 _steamos_commit=96053cb77390d18cc76cebead210481ea173bee5
 
-wget https://raw.githubusercontent.com/ValveSoftware/steamos_kernel/${_steamos_commit}/drivers/input/joystick/xpad.c
+_pkgver=0.1
+_pkgpath=/usr/src/${_pkgname}-${_pkgver}
+
+wget https://raw.githubusercontent.com/ValveSoftware/steamos_kernel/${_steamos_commit}/drivers/input/joystick/xpad.c 
 
 patch xpad.c -Np4 -i change-name.patch
 
-#sudo dkms uninstall -m ${_pkgname} -v ${_pkgver}
-#sudo dkms remove -m ${_pkgname} -v ${_pkgver} --all
+#TODO: uninstall/remove existing version
+#sudo dkms uninstall ${_pkgname}/${_pkgver}
+#sudo dkms remove ${_pkgname}/${_pkgver} --all
 
-echo "installing source"
 sudo mkdir -p ${_pkgpath}
 sudo cp xpad.c ${_pkgpath}/steamos-xpad.c
 sudo cp makefile.dkms ${_pkgpath}/Makefile
